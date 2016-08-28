4c1c3358ead497abf9ba20c0e255258a0c9a437f
Srinivas Sunkari <srinivas.sunkari@laterooms.com>
2016-08-28 12:59:53 +0100

    Added Login page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+router.get('/patient_register', function (req, res, next) {
+    res.render('patient_register', {
+        title: 'Patient Signup'
+    });
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login', {
+        title: 'Login'
+    });
 });
 
 module.exports = router;
